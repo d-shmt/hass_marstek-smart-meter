69284217234e2bef7f870370d286ec5893163823
d-shmt <91471929+d-shmt@users.noreply.github.com>
2025-07-24 16:56:45 +0200

    Update __init__.py

--- custom_components/marstek_ct/__init__.py
+++ custom_components/marstek_ct/__init__.py
@@ -1,30 +1,26 @@
 """The Marstek CT Meter integration."""
 from __future__ import annotations
-
 from datetime import timedelta
 import logging
-
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_HOST, CONF_MAC, CONF_TYPE, Platform
+from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
-
 from .api import MarstekCtApi
 from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
+# Die BINARY_SENSOR-Plattform wird hier wieder entfernt
 PLATFORMS: list[Platform] = [Platform.SENSOR]
-
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Marstek CT Meter from a config entry."""
-    
     api = MarstekCtApi(
-        host=entry.data[CONF_HOST],
+        host=entry.data["host"],
         device_type=entry.data["device_type"],
         battery_mac=entry.data["battery_mac"],
-        ct_mac=entry.data[CONF_MAC],
-        ct_type=entry.data[CONF_TYPE],
+        ct_mac=entry.data["ct_mac"],
+        ct_type=entry.data["ct_type"],
     )
 
     async def async_update_data():
@@ -32,7 +28,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         try:
             data = await hass.async_add_executor_job(api.fetch_data)
             if "error" in data:
-                raise UpdateFailed(f"Error communicating with API: {data['error']}")
+                raise UpdateFailed(f"API error: {data['error']}")
             return data
         except Exception as err:
             raise UpdateFailed(f"Error communicating with API: {err}")
@@ -42,21 +38,16 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         _LOGGER,
         name="marstek_ct_sensor",
         update_method=async_update_data,
-        update_interval=timedelta(seconds=5),
+        update_interval=timedelta(seconds=30),
     )
 
     await coordinator.async_config_entry_first_refresh()
-
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
-
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-
     return True
-
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
         hass.data[DOMAIN].pop(entry.entry_id)
-
     return unload_ok
